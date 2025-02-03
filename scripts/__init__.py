b1a7d03091bf3b5119cded535f6b24a65cde670e
Christian Gaser <christian.gaser@uni-jena.de>
2025-02-03 08:45:02 +0100

    Slightly changed defaults for T1Prep

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -1,3 +1,3 @@
 from .segment import run_segment
-from .utils import progress_bar, remove_file, correct_bias_field, get_atlas, resample_and_save_nifti, get_resampled_header, get_partition, align_brain
+from .utils import progress_bar, remove_file, correct_bias_field, get_atlas, resample_and_save_nifti, get_resampled_header, get_partition, align_brain, cleanup, get_cerebellum
 
