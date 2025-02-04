8d8b046d977bf3cf89e89675e76d1a73c0a00ff6
Christian Gaser <christian.gaser@uni-jena.de>
2025-02-04 23:01:32 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,28 +1,9 @@
-#!/usr/bin/env python
+# https://setuptools.pypa.io/en/latest/userguide/pyproject_config.html
+# If compatibility with legacy builds or versions of tools that don’t support
+# certain packaging standards (e.g. PEP 517 or PEP 660), a simple setup.py
+# script can be added to your project [1] (while keeping the configuration in
+# pyproject.toml):
 
-import sys
-import setuptools
+from setuptools import setup
 
-python_version = sys.version[:3]
-
-if (python_version != '3.8') & (python_version != '3.9'):
-    raise Exception('Setup.py only works with python version 3.8 or 3.9, not {}'.format(python_version))
-
-else:
-
-    with open('requirements_python' + python_version + '.txt') as f:
-        required_packages = [line.strip() for line in f.readlines()]
-
-    print(setuptools.find_packages())
-
-    setuptools.setup(name='T1Prep',
-                     version='0.1.0',
-                     license='Apache 2.0',
-                     description='T1 PREProcessing Pipeline (aka PyCAT)',
-                     author='Christian Gaser <christian.gaser@uni-jena.de>',
-                     url='https://github.com/ChristianGaser/T1Prep',
-                     keywords=['segmentation', 'domain-agnostic', 'brain'],
-                     packages=setuptools.find_packages(),
-                     python_requires='>=3.8',
-                     install_requires=required_packages,
-                     include_package_data=True)
+setup()
